e5170313c190d631097285f2fdfb9349562dc99a
ait <ait@ex.in>
2020-02-29 20:22:09 +0900

    dn

--- app.js
+++ app.js
@@ -2,14 +2,21 @@
   const el = document.querySelector(".content");
   el.insertAdjacentHTML("beforeend", `<p>テスト：テスト${null}`);
 
-  const numbers = [1, 4, 7, 8, 10];
+  const numbers = [1, 4, 7, 8, 9, 10];
 
-  const evenNum = numbers.filter(num => {
-    if (num % 2 === 0) {
-      return true;
-    } else return false;
-  });
+  // const evenNum = numbers.filter(num => {
+  //   if (num % 2 === 0) {
+  //     return true;
+  //   } else return false;
+  // });
 
+  // 式自体を return
+  // const evenNum = numbers.filter(num => {
+  //   return num % 3 === 0;
+  // });
+
+  // さらに短く
+  const evenNum = numbers.filter(num => num % 3 === 0)
+  
   el.insertAdjacentHTML("beforeend", `<h2>even：${evenNum}</h2>`);
-
 }
