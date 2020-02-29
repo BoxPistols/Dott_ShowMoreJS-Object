1c24cb7167d9e1bbec2c46d5c497f5e6244cca58
ait <ait@ex.in>
2020-02-29 17:18:57 +0900

    スプレッド構文

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 {
   const el = document.querySelector('.content')
 
-  const score = [80, 90, 40, 70]
-  console.log(`Score: ${score}`)
+  const otherScores = [22, 33, 44, 55] // 他の配列追加
+  const scores = [80, 90, 40, 70, ...otherScores] // 末尾に配列追加
+  console.log(`Score: ${scores}`)
+  // scores.splice(0,1,120)
+  // scores.push(111, 999)
+  // scores.shift() // 単一
 
-  for (let i = 0; i < score.length ; i++) {
-    el.insertAdjacentHTML('afterbegin', (`<h2>Score: ${score[i]}</h2>`) )
+  for (let i = 0; i < scores.length ; i++) {
+    el.insertAdjacentHTML('beforeend', (`<h2>Score ${i+1}: ${scores[i]}</h2>`) )
   }
+
+  // 計算用の関数
+  function sum(a, b, c, d){
+    el.insertAdjacentHTML('beforeend', (`<h2> 合計： ${a + b + c + d}</h2>`) )
+  }
+
+  sum(...otherScores);
 }
