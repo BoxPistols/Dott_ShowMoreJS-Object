cfd29fc21d9d5cac8e945bb0464d7d79955d5371
ait <ait@ex.in>
2020-02-29 19:32:08 +0900

    foEach ベース

--- app.js
+++ app.js
@@ -2,28 +2,14 @@
   const el = document.querySelector(".content");
 
   const otherScores = [22, 33, 44, 55]; // 他の配列追加
-  const scores = [80, 90, 40, 70, ...otherScores]; // 末尾に配列追加
+  const scores = [80, 90, 40, 70]; // 末尾に配列追加
 
-  for (let i = 0; i < scores.length; i++) {
-    el.insertAdjacentHTML("beforeend", `<li>Score ${i + 1}: ${scores[i]}</li>`);
-  }
-  // 計算用の関数
-  function sum(x, y, z, a) {
-    el.insertAdjacentHTML("beforeend", `<h3> 合計： ${x + y + z + a}</h3>`);
-  }
-  sum(...scores);
+  scores.forEach((x, index) =>{
+    el.insertAdjacentHTML("beforeend", `<p> 評価：${index + 1} = ${x}</p>`);
+  })
+  // for (let i = 0; i < scores.length; i++) {
+  //   el.insertAdjacentHTML("beforeend", `<li>Score ${i + 1}: ${scores[i]}</li>`);
+  // }
 
-
-  const [a,b, ...others] = scores
-  el.insertAdjacentHTML("beforeend", `<h3> 分割代入： ${a} ${b} ${others}</h3>`);
-
-  // scores.forEach(function(x) {
-  //   document.write(x);
-  // });
-  //
-  // const a = [1, 2, 3];
-  // a.forEach((value, i) => {
-  //   document.write(i, value);
-  // });
 
 }
