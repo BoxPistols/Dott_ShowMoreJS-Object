a927d02b7a57df17b6e89ba4c1c5cb02f481f62a
ait <ait@ex.in>
2020-02-29 20:39:34 +0900

    Object 取得

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 {
   const el = document.querySelector(".content");
-  el.insertAdjacentHTML("beforeend", `<p>テスト：テスト${null}`);
+  // el.insertAdjacentHTML("beforeend", `<p>テスト：テスト${null}`);
 
-  const numbers = [1, 4, 7, 8, 9, 10];
+  const point = {
+    x: 100,
+    y: 180,
+    n: 120,
+  };
 
-  // const evenNum = numbers.filter(num => {
-  //   if (num % 2 === 0) {
-  //     return true;
-  //   } else return false;
-  // });
+  point.x = 120;
+  point.z = 90;
 
-  // 式自体を return
-  // const evenNum = numbers.filter(num => {
-  //   return num % 3 === 0;
-  // });
+  // delete point.z
+  // console.log(point)
 
-  // さらに短く
-  const evenNum = numbers.filter(num => num % 3 === 0)
-  
-  el.insertAdjacentHTML("beforeend", `<h2>even：${evenNum}</h2>`);
+  el.insertAdjacentHTML("beforeEnd", `<p>キーからプロパティを取得：${point.z}`);
+
+  for (var i = 0; i < Object.keys(point).length; i++) {
+    el.insertAdjacentHTML(
+      "beforeEnd",
+      `<p>Object =
+      ${Object.keys(point)[i]}:
+      ${Object.values(point)[i]}`
+    );
+  }
 }
