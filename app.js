cfef7633e9c69a6ec4bfb641d1ca66f96562ae78
ait <ait@ex.in>
2020-02-29 19:46:33 +0900

    dn

--- app.js
+++ app.js
@@ -4,12 +4,23 @@
   const otherScores = [22, 33, 44, 55]; // 他の配列追加
   const scores = [80, 90, 40, 70]; // 末尾に配列追加
 
-  scores.forEach((x, index) =>{
+  scores.forEach((x, index) => {
     el.insertAdjacentHTML("beforeend", `<p> 評価：${index + 1} = ${x}</p>`);
-  })
+  });
   // for (let i = 0; i < scores.length; i++) {
   //   el.insertAdjacentHTML("beforeend", `<li>Score ${i + 1}: ${scores[i]}</li>`);
   // }
+  //
+  // const upDateScores = scores.map((s) => {
+  //   return s * 1.5
+  // })
 
+  const upDateScores = scores.map(s => s + 20);
 
+  console.log(upDateScores);
+  el.insertAdjacentHTML("beforeend", `<p>upDateScores：${upDateScores}`);
+
+  const prices = [180, 190, 200];
+  const updatedPrices = prices.map(price => price + 20);
+  console.log(updatedPrices);
 }
